e29d6d89d4e4fda62f731269bea45336184fbf90
maria-virginia <mvmendonca.s3301@gmail.com>
2024-09-26 16:16:14 -0300

    update

--- app/api/auth/postLogin.js
+++ app/api/auth/postLogin.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function postLogin(login, password){
-    const url = "http://localhost:8081/auth/login"; // URL completo do backend
+    const url = "http://localhost:8081/auth/login"; 
     return await axios.post(url, {
         username: login,
         password: password,

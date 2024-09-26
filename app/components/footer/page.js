fd0cf9d638483e2bbdc5745356c1dafe6074848d
beatrizAVanderlei <anabiaavanderlei@gmail.com>
2024-09-26 15:27:48 -0300

    update footer

--- app/components/footer/page.js
+++ app/components/footer/page.js
@@ -10,11 +10,13 @@ const Footer = () => {
         return null
     }
     return (
-      <footer className="bg-dark text-white py-2 px-1 mt-auto">
-        <p className="p-0 m-0">© 2024 Agiota. All rights reserved.</p>
-      </footer>
+        <footer className="bg-dark text-white py-2 px-1 mt-auto">
+            <p className="p-0 m-0">© 2024.1 A.G.I.O.T.A All rights reserved.</p>
+            <p className="p-0 m-0">Programação Web 2024.1</p>
+            <p className="p-0 m-0">Ana Beatriz | Caio Vinicius | Jonas Junior | Maria Virginia</p>
+        </footer>
     );
-  };
-  
-  export default Footer;
+};
+
+export default Footer;
   
